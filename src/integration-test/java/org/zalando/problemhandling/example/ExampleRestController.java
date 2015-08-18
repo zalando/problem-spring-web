b7f85f20ec14fc7a14f5e046e9523c206aed3884
Lukas Niemeier <lukas.niemeier@zalando.de>
2015-08-18 23:46:37 +0200

    Draft of the library

---
+++ src/integration-test/java/org/zalando/problemhandling/example/ExampleRestController.java
@@ -0,0 +1,49 @@
+package org.zalando.problemhandling.example;
+
+/*
+ * #%L
+ * problem-handling
+ * %%
+ * Copyright (C) 2015 Zalando SE
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ExampleRestController {
+
+    @RequestMapping(value = "/handler-throwable", method = RequestMethod.GET)
+    public ResponseEntity<String> throwable() {
+        throw new RuntimeException("expected");
+    }
+
+    @RequestMapping(value = "/handler-problem", method = RequestMethod.GET)
+    public ResponseEntity<String> problem() {
+        throw new ExpectedProblem();
+    }
+
+    @RequestMapping(value = "/handler", method = RequestMethod.PUT)
+    public ResponseEntity<String> put(@RequestBody final String body) {
+        return ResponseEntity.ok(body);
+    }
+
+}
---
+++ src/test/java/org/zalando/problemhandling/MediaTypesTest.java
@@ -0,0 +1,46 @@
+package org.zalando.problemhandling;
+
+/*
+ * #%L
+ * problem-handling
+ * %%
+ * Copyright (C) 2015 Zalando SE
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+
+import org.junit.Test;
+import org.springframework.http.MediaType;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.aMapWithSize;
+import static org.hamcrest.core.Is.is;
+import static org.hobsoft.hamcrest.compose.ComposeMatchers.hasFeature;
+
+public class MediaTypesTest {
+
+    @Test
+    public void isApplicationProblemJson() {
+        assertThat(MediaTypes.PROBLEM, hasFeature("Type", MediaType::getType, is("application")));
+        assertThat(MediaTypes.PROBLEM, hasFeature("Subtype", MediaType::getSubtype, is("problem+json")));
+        assertThat(MediaTypes.PROBLEM, hasFeature("Parameters", MediaType::getParameters, is(aMapWithSize(0))));
+    }
+
+    @Test
+    public void isCompatibleWithApplicationJsonWildcard() {
+        assertThat(MediaTypes.PROBLEM.isCompatibleWith(MediaType.parseMediaType("application/*+json")), is(true));
+    }
+
+}
