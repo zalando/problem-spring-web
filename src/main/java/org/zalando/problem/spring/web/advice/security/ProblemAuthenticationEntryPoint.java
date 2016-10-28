cf193137666b83698c5c938c099dc68e73f175f3
Willi Schönborn <willi.schoenborn@zalando.de>
2016-10-28 09:06:45 +0200

    Added comment to clarify bean name

--- src/main/java/org/zalando/problem/spring/web/advice/security/ProblemAuthenticationEntryPoint.java
+++ src/main/java/org/zalando/problem/spring/web/advice/security/ProblemAuthenticationEntryPoint.java
@@ -6,12 +6,19 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ * AuthenticationEntryPoint forwarding authentication exceptions to spring-webmvc's HandlerExceptionResolver as defined
+ * by WebMvcConfigurationSupport. Compatible with spring-webmvc 4.3.3.
+ *
+ * @see WebMvcConfigurationSupport#handlerExceptionResolver()
+ */
 @Component
 public class ProblemAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
