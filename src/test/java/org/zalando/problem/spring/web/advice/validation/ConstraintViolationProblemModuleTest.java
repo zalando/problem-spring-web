010f3bfa9283f286e6416c938e96c80bcd03219e
Willi Schönborn <willi.schoenborn@zalando.de>
2017-05-11 23:43:05 +0200

    Added test for custom type

--- src/test/java/org/zalando/problem/spring/web/advice/validation/ConstraintViolationProblemModuleTest.java
+++ src/test/java/org/zalando/problem/spring/web/advice/validation/ConstraintViolationProblemModuleTest.java
@@ -1,4 +1,4 @@
-package org.zalando.problem.validation;
+package org.zalando.problem.spring.web.advice.validation;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.MapperFeature;
@@ -6,10 +6,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.zalando.problem.ProblemModule;
-import org.zalando.problem.spring.web.advice.validation.ConstraintViolationProblem;
-import org.zalando.problem.spring.web.advice.validation.Violation;
+import org.zalando.problem.validation.ConstraintViolationProblemModule;
+
+import java.net.URI;
 
 import static com.jayway.jsonassert.JsonAssert.with;
+import static java.util.Collections.emptyList;
 import static java.util.Collections.singletonList;
 import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
 import static org.hamcrest.Matchers.contains;
@@ -20,7 +22,7 @@ import static org.hamcrest.Matchers.is;
 public class ConstraintViolationProblemModuleTest {
 
     @Test
-    public void shouldSerialize() throws JsonProcessingException {
+    public void shouldSerializeWithoutAutoDetect() throws JsonProcessingException {
         final ObjectMapper mapper = new ObjectMapper()
                 .disable(MapperFeature.AUTO_DETECT_FIELDS)
                 .disable(MapperFeature.AUTO_DETECT_GETTERS)
@@ -38,6 +40,19 @@ public class ConstraintViolationProblemModuleTest {
                 .assertThat("violations", hasSize(1))
                 .assertThat("violations.*.field", contains("bob"))
                 .assertThat("violations.*.message", contains("was missing"));
+    }
+
+    @Test
+    public void shouldSerializeCustomType() throws JsonProcessingException {
+        final ObjectMapper mapper = new ObjectMapper()
+                .registerModule(new ProblemModule())
+                .registerModule(new ConstraintViolationProblemModule());
+
+        final URI type = URI.create("foo");
+        final ConstraintViolationProblem unit = new ConstraintViolationProblem(type, BAD_REQUEST, emptyList());
+
+        with(mapper.writeValueAsString(unit))
+                .assertThat("type", is("foo"));
     }
 
 }
