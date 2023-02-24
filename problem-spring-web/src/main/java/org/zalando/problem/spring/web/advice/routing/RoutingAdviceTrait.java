ea317757ff7e60e1f9d29bf6e84612149adb711e
drohe <daniel.rohe@zalando.de>
2023-02-24 17:01:44 +0100

    removed handling of NoHandlerFoundException

--- problem-spring-web/src/main/java/org/zalando/problem/spring/web/advice/routing/RoutingAdviceTrait.java
+++ problem-spring-web/src/main/java/org/zalando/problem/spring/web/advice/routing/RoutingAdviceTrait.java
@@ -11,6 +11,5 @@ import static org.apiguardian.api.API.Status.STABLE;
 @API(status = STABLE)
 public interface RoutingAdviceTrait extends
         MissingServletRequestParameterAdviceTrait,
-        NoHandlerFoundAdviceTrait,
         ServletRequestBindingAdviceTrait {
 }
